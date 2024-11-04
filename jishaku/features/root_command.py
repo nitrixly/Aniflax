64080c260759538c6b1c97b05df00af7fe42d1b8
Nitrix <126798153+nitrixly@users.noreply.github.com>
2024-11-04 22:42:12 +0530

    Update root_command.py

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -69,11 +69,8 @@ class RootCommand(Feature):
             dist_version = f'unknown `{discord.__version__}`'
 
         summary = [
-            f"Aniflax v{package_version('jishaku')}, {dist_version}, "
-            f"`Python {sys.version}` on `{sys.platform}`".replace("\n", ""),
-            f"Module was loaded <t:{self.load_time.timestamp():.0f}:R>, "
-            f"cog was loaded <t:{self.start_time.timestamp():.0f}:R>.",
-            ""
+            f"Aniflax v{package_version('jishaku')}, {dist_version}, `Python {sys.version.split()[0]}` on `{sys.platform}`",
+            f"Process started at <t:{int(self.load_time.timestamp())}:R>, bot was ready at <t:{int(self.start_time.timestamp())}:R>.",
         ]
 
         # detect if [procinfo] feature is installed
@@ -84,77 +81,37 @@ class RootCommand(Feature):
                 with proc.oneshot():
                     try:
                         mem = proc.memory_full_info()
-                        summary.append(f"Using {natural_size(mem.rss)} physical memory and "
-                                       f"{natural_size(mem.vms)} virtual memory, "
-                                       f"{natural_size(mem.uss)} of which unique to this process.")
+                        summary.append(f"Using {natural_size(mem.rss)} at this process.")
                     except psutil.AccessDenied:
                         pass
 
                     try:
-                        name = proc.name()
                         pid = proc.pid
-                        thread_count = proc.num_threads()
-
-                        summary.append(f"Running on PID {pid} (`{name}`) with {thread_count} thread(s).")
+                        summary.append(f"Running on PID {pid}")
                     except psutil.AccessDenied:
                         pass
-
-                    summary.append("")  # blank line
             except psutil.AccessDenied:
                 summary.append(
                     "psutil is installed, but this process does not have high enough access rights "
                     "to query process information."
                 )
-                summary.append("")  # blank line
-        s_for_guilds = "" if len(self.bot.guilds) == 1 else "s"
-        s_for_users = "" if len(self.bot.users) == 1 else "s"
-        cache_summary = f"{len(self.bot.guilds)} guild{s_for_guilds} and {len(self.bot.users)} user{s_for_users}"
 
-        # Show shard settings to summary
-        if isinstance(self.bot, discord.AutoShardedClient):
-            if len(self.bot.shards) > 20:
-                summary.append(
-                    f"This bot is automatically sharded ({len(self.bot.shards)} shards of {self.bot.shard_count})"
-                    f" and can see {cache_summary}."
-                )
-            else:
-                shard_ids = ', '.join(str(i) for i in self.bot.shards.keys())
-                summary.append(
-                    f"This bot is automatically sharded (Shards {shard_ids} of {self.bot.shard_count})"
-                    f" and can see {cache_summary}."
-                )
-        elif self.bot.shard_count:
-            summary.append(
-                f"This bot is manually sharded (Shard {self.bot.shard_id} of {self.bot.shard_count})"
-                f" and can see {cache_summary}."
-            )
-        else:
-            summary.append(f"This bot is not sharded and can see {cache_summary}.")
-
-        # pylint: disable=protected-access
-        if self.bot._connection.max_messages:  # type: ignore
-            message_cache = f"Message cache capped at {self.bot._connection.max_messages}"  # type: ignore
-        else:
-            message_cache = "Message cache is disabled"
-
-        remarks = {
-            True: 'enabled',
-            False: 'disabled',
-            None: 'unknown'
-        }
-
-        *group, last = (
-            f"{intent.replace('_', ' ')} intent is {remarks.get(getattr(self.bot.intents, intent, None))}"
-            for intent in
-            ('presences', 'members', 'message_content')
+        guild_count = len(self.bot.guilds)
+        user_count = len(self.bot.users)
+        summary.append(
+            f"This bot is not sharded and can see {guild_count} guild{'s' if guild_count != 1 else ''} and {user_count} user{'s' if user_count != 1 else ''}."
         )
 
-        summary.append(f"{message_cache}, {', '.join(group)}, and {last}.")
+        intent_summary = {
+            'GuildPresences': 'enabled' if self.bot.intents.presences else 'disabled',
+            'GuildMembers': 'enabled' if self.bot.intents.members else 'disabled',
+            'MessageContent': 'enabled' if self.bot.intents.message_content else 'disabled',
+        }
+        summary.extend(
+            [f"`{intent}` intent is {status}" for intent, status in intent_summary.items()]
+        )
 
-        # pylint: enable=protected-access
-
-        # Show websocket latency in milliseconds
-        summary.append(f"Average websocket latency: {round(self.bot.latency * 1000, 2)}ms")
+        summary.append(f"Average websocket latency: {round(self.bot.latency * 1000)}ms")
 
         await ctx.send("\n".join(summary))
 
